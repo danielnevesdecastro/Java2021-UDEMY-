26f312744189eb29d6c6df54c5818830da0c9923
Daniel Neves <danielnevesdecastro@gmail.com>
2023-01-04 16:41:58 -0300

    While, For

---
+++ exercícios/src/controle/DesafioDiaSemana.java
@@ -0,0 +1,31 @@
+package controle;
+
+import java.util.Scanner;
+
+//Aula 67,68  Desafio 
+public class DesafioDiaSemana {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Informa o dia da semana: ");
+		String dia = scan.next();
+
+		if (dia.equalsIgnoreCase("domingo")) {
+			System.out.println("1");
+		} else if ("segunda".equalsIgnoreCase(dia)) { // outra forma de declara a função equals
+			System.out.println("2");
+		} else if ("terça".equalsIgnoreCase(dia)) {
+			System.out.println("3");
+		} else if ("quarta".equalsIgnoreCase(dia)) {
+			System.out.println("4");
+		} else if ("quinta".equalsIgnoreCase(dia)) {
+			System.out.println("5");
+		} else if ("sexta".equals(dia.toLowerCase())) { // mais uma forma , . to lower case converte o valor pra letrtas
+			System.out.println("6");					// minusculas para comparaR
+		} else {
+			System.out.println("Opção invalida ! ");
+		}
+
+		scan.close();
+	}
+}
---
+++ exercícios/src/controle/WhileDeterminado.java
@@ -0,0 +1,13 @@
+package controle;
+
+//Aula69, While Determinado
+public class WhileDeterminado {
+	public static void main(String[] args) {
+		int cont = 1;
+
+		while (cont <= 10) {
+			System.out.printf("Bom dia Nº %d\n", cont);
+			cont += 2;
+		}
+	}
+}
