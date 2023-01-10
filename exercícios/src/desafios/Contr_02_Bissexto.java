9609c50aead8d169d8f4b712f6dc4ac0a84ea59e
Daniel Neves <danielnevesdecastro@gmail.com>
2023-01-09 23:56:52 -0300

    Desafios Estrutura de Controle

--- exercícios/src/desafios/Contr_02_Bissexto.java
+++ exercícios/src/desafios/Contr_02_Bissexto.java
@@ -11,22 +11,17 @@ public class Contr_02_Bissexto {
 
 		System.out.print("Informe o Ano:");
 		int ano = scan.nextInt();
-		String bissexto = ano % 4 == 0 && ((ano % 100) != 0 || ano % 400 == 0) ? 
-				"Ano Bissexto!" : "Não é ano Bissexto ! ";
+		String bissexto = ano % 4 == 0 && ((ano % 100) != 0 || ano % 400 == 0) ? "Ano Bissexto!"
+				: "Não é ano Bissexto ! ";
 
-		System.out.println(bissexto);
-
-		// Versão do professor
-		/*
-		boolean bissexto2 = ano % 4 == 0 && ((ano % 100) != 0 || ano % 400 == 0);
-		if (bissexto2 == true) {
-			bissexto = "Ano bissexto!";
-		} else {
-			bissexto = "Não é Bissexto!";
-		}
 		System.out.println(bissexto);
 		scan.close();
-		*/
+		// Versão do professor
+		/*
+		 * boolean bissexto2 = ano % 4 == 0 && ((ano % 100) != 0 || ano % 400 == 0); if
+		 * (bissexto2 == true) { bissexto = "Ano bissexto!"; } else { bissexto =
+		 * "Não é Bissexto!"; } System.out.println(bissexto); scan.close();
+		 */
 	}
 
 }
