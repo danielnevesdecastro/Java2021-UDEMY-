6ae486c358c40d673e432380c46458b321dbf4e9
Daniel Neves <danielnevesdecastro@gmail.com>
2023-01-21 22:41:48 -0300

    Métodos

--- exercícios/src/classe/Data.java
+++ exercícios/src/classe/Data.java
@@ -5,4 +5,16 @@ public class Data {
 
 	int dia, mes, ano;
 
+	String dataFormatada() {
+		String dataF = dia + " / " + mes + " / " + ano + ".";
+
+		return dataF;
+	}
+
+	String dataExtenso() {
+		String dataEx ="Data: "+ dia + ", de " + mes +" de "+ ano ;
+
+		return dataEx;
+
+	}
 }
