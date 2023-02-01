c1b3cd0a9ba008ae71015ae177d4b7b019157a1d
Daniel Neves <danielnevesdecastro@gmail.com>
2023-01-31 23:18:27 -0300

    Collections - Set

---
+++ exercícios/src/colecoes/ConjuntoBaguncado.java
@@ -0,0 +1,55 @@
+package colecoes;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoBaguncado {
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public static void main(String[] args) {
+
+		HashSet conjunto = new HashSet();
+
+		conjunto.add(1.2); // double casting Double (wrappers)
+		conjunto.add(true);// wrappers
+		conjunto.add("Teste");
+		// conjunto.add("teste");
+		conjunto.add(1);
+		conjunto.add('x');
+
+		// size = tamanho do conjunto
+		System.out.println("Tamanho é: " + conjunto.size());
+
+		// remove = remover elementos do conjuntos, retorna true or false
+		System.out.println(conjunto.remove("teste"));
+		System.out.println(conjunto.remove("Teste"));
+		System.out.println(conjunto.remove('x'));
+		System.out.println("Tamanho é: " + conjunto.size());
+
+		// contains , encontrar se determinado elemento está condito no conjunto
+		//  retorna boolean
+		System.out.println(conjunto.contains('x'));
+		System.out.println(conjunto.contains(1));
+		System.out.println(conjunto.contains(false)); 
+		
+		//variavel do tipo set ,a tribuindo hashset
+		//POO ??
+		Set nums = new HashSet();
+		
+		nums.add(1);
+		nums.add(2);
+		nums.add(3);
+		
+		//imprimindo valores 
+		System.out.println(conjunto);
+		System.out.println(nums);
+		
+		
+		//conjunto.addAll(nums);//união conjuntos
+		conjunto.retainAll(nums);//interseção dos valores entre conjuntos (conj iguais)
+		System.out.println(conjunto);
+		
+		conjunto.clear();// limpar conjunto
+		System.out.println(conjunto);
+		
+	}
+}
