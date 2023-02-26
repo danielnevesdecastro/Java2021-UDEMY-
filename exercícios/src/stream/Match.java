1dd5901ccd214333ec134c92c281845c02504067
Daniel Neves <danielnevesdecastro@gmail.com>
2023-02-25 21:47:22 -0300

    Match

---
+++ exercícios/src/stream/Match.java
@@ -0,0 +1,22 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+//209 - Match
+public class Match {
+	public static void main(String[] args) {
+		Aluno a1 = new Aluno("Ana", 7.1);
+		Aluno a2 = new Aluno("Luna", 7.1);
+		Aluno a3 = new Aluno("Gui", 8.1);
+		Aluno a4 = new Aluno("Gabi", 10);
+
+		List<Aluno> alunos = Arrays.asList(a1, a2, a3, a4);
+
+		Predicate<Aluno> aprovados = a -> a.nota >= 7;
+		System.out.println(alunos.stream().allMatch(aprovados));
+		System.out.println(alunos.stream().anyMatch(aprovados));
+		System.out.println(alunos.stream().noneMatch(aprovados));
+	}
+}
